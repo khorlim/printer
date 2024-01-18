8738c1a02d24fe2108c3b74e6062b68397f7ac6c
khorlim <khorlim2001@gmail.com>
2024-01-18 14:11:18 +0800

    fix

--- print_function.dart
+++ print_function.dart
@@ -3,7 +3,9 @@ import 'package:tunaipro/extra_utils/printer/super_printer.dart';
 import 'package:tunaipro/general_module/order_module/import_path.dart';
 
 Future<bool> printReceipt(
-    {required BuildContext context, required ReceiptData receiptData}) async {
+    {required BuildContext context,
+    required ReceiptData receiptData,
+    bool openDrawer = false}) async {
   // final SuperPrinter superPrinter = SuperPrinter();
   // bool success = await superPrinter.startPrint(
   //     receiptData: receiptData, receiptType: ReceiptType.beauty);
