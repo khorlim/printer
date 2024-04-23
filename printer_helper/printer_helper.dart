b466235b2e9ffc00563bc2d58028b1ab58f2655e
khorlim <khorlim2001@gmail.com>
2024-04-23 17:03:59 +0800

    fix

---
+++ printer_helper/printer_helper.dart
@@ -0,0 +1,32 @@
+import 'package:tunaipro/engine/receipt/model/receipt_data.dart';
+import 'package:tunaipro/extra_utils/printer/src/print_command.dart';
+import 'package:tunaipro/extra_utils/printer/super_printer.dart';
+
+class PrinterHelper {
+  PrinterHelper._();
+  factory PrinterHelper() => _instance;
+  static final PrinterHelper _instance = PrinterHelper._();
+
+  final SuperPrinter superPrinter = SuperPrinter();
+
+  Future<bool> printSaleReceipt({
+    required ReceiptData receiptData,
+    ReceiptType receiptType = ReceiptType.beauty,
+    bool openDrawer = false,
+  }) async {
+    bool success = await superPrinter.printReceipt(
+      receiptData: receiptData,
+      receiptType: receiptType,
+      openDrawer: openDrawer,
+    );
+    return success;
+  }
+
+  Future<bool> printCustomCommand(SuperPrintCommand printCommand) async {
+    return superPrinter.startPrint(printCommand);
+  }
+
+  Future<void> openDrawer() {
+    return superPrinter.openDrawer();
+  }
+}
