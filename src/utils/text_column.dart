437659417dcfb6ba446e57e5797c0191c5492fd3
khorlim <khorlim2001@gmail.com>
2025-05-09 15:41:10 +0800

    fix

--- src/utils/text_column.dart
+++ src/utils/text_column.dart
@@ -1,16 +1,21 @@
 import 'package:flutter_esc_pos_utils/flutter_esc_pos_utils.dart';
 
+enum ColumnOverflow { truncate, wrap }
+
 class TextColumn {
   final String text;
   final int ratio;
   final bool bold;
   final PosAlign alignment;
+  final ColumnOverflow overflow;
 
-  TextColumn(
-      {required this.text,
-      required this.ratio,
-      this.bold = false,
-      this.alignment = PosAlign.left});
+  TextColumn({
+    required this.text,
+    required this.ratio,
+    this.bold = false,
+    this.alignment = PosAlign.left,
+    this.overflow = ColumnOverflow.truncate,
+  });
 
   PosColumn toPosColumn(int width) {
     return PosColumn(
