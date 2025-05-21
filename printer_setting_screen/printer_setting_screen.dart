38577626f84bb8a524bd1f256d3032ef67764148
khorlim <khorlim2001@gmail.com>
2025-05-21 19:23:08 +0800

    up

---
+++ printer_setting_screen/printer_setting_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PrinterSettingScreen extends StatelessWidget {
+  const PrinterSettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Printer Setting Screen')),
+    );
+  }
+}
