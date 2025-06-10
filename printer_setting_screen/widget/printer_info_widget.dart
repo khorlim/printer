6c5e0b55f41e4b18214577f878ad59fbf40462f7
Izzudin Zamri <101189465+kudin0@users.noreply.github.com>
2025-06-10 14:46:55 +0800

    refactor: update PrinterInfoWidget to use localized string and improve styling

--- printer_setting_screen/widget/printer_info_widget.dart
+++ printer_setting_screen/widget/printer_info_widget.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/widgets.dart';
 
+import '../../../../tunai_style/extension/build_context_extension.dart';
+import '../../../../tunai_style/translation/strings.g.dart';
+
 class PrinterInfoWidget extends StatelessWidget {
   const PrinterInfoWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('No Printer');
+    return Text(
+      t.noPrinter,
+      style: context.text.primary.copyWith(
+        color: context.colorScheme.onSurfaceVariant,
+      ),
+    );
   }
 }
