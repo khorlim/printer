ebe6f076907d87c02e5f2f1d2deb69ff11eeffba
khorlim <khorlim2001@gmail.com>
2025-06-24 14:19:51 +0800

    up

--- widget/change_ip_dialog.dart
+++ widget/change_ip_dialog.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../../../core_utils/tunai_navigator/tunai_navigator.dart';
-import '../../../share_code/custom_dialog/dialog_manager/dialog_manager.dart';
-import '../../../share_code/shared_widgets/text_field/my_text_field.dart';
+import '../../../tunai_style/widgets/custom_dialog/dialog_manager/dialog_manager.dart';
+import '../../../tunai_style/widgets/shared_widgets/text_field/my_text_field.dart';
 import '../../../tunai_style/style_imports.dart';
 import '../../../tunai_style/common_widgets/common_widgets.dart';
 
